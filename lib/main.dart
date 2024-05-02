6d797ef327692a0a551ea53e627c58ade36c93a0
soltani-ard <behnamsoltani.ard@gmail.com>
2024-05-02 06:53:03 -0700

    Home Screen UI Completed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(child: Text('Hello world'),),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
